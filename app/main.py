81c35e63ec7e10aeefbf4a283d873929263a84e6
jakubslicner <100052903+jakubslicner@users.noreply.github.com>
2025-10-02 21:56:04 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from app.routes import health
+
+app = FastAPI(title="Rachmistrz Backend")
+
+# rejestrujemy routery
+app.include_router(health.router)
+
+@app.get("/")
+def root():
+    return {"message": "Hello from Rachmistrz Backend!"}
 
